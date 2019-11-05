81b16aecb46d4217b9238ee3c055312272d3561b
Bruno Carneiro <brunocarneirofs@gmail.com>
2019-11-05 12:06:23 -0300

    add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-54779307-1`,
       },
     },
     `gatsby-plugin-feed`,
